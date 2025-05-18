a8d5bb507032e6ae0c5db29909cb75a8e0576a27
Game-On-Dp <gorbach0864213579@gmail.com>
2025-05-18 23:17:57 +0300

    make some models

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Worker(AbstractUser):
+    position =  models.ForeignKey(Position,on_delete=models.CASCADE)
+
+class Task(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    priority =
