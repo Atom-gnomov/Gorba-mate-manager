99b7f089e45e222645ef96484c470c08ca3d570a
Atom-gnomov <gorbach0864213579@gmail.com>
2025-06-03 08:30:46 +0300

    Worked and complete ADD login and registration next step is required loginmixin fully complete now have search login logout reg
    rename project to GorbaMateManagerProject
    add readme file

--- GorbaMateManagerProject/asgi.py
+++ GorbaMateManagerProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoProject1 project.
+ASGI config for GorbaMateManagerProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoProject1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GorbaMateManagerProject.settings')
 
 application = get_asgi_application()
